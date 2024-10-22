52a639a30b7f54b93ad1c2c5d4f70012477693e1
Srinivasarao Dharnasi <Srinivasa.Rao.Dharnasi@smartrain.net>
2024-10-22 17:55:18 +0530

    version-1.0: did the changes

--- android/app/build/generated/source/buildConfig/release/com/uesiap/geethavali/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/com/uesiap/geethavali/BuildConfig.java
@@ -7,8 +7,8 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.uesiap.geethavali";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 125;
-  public static final String VERSION_NAME = "12.5";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "1.0";
   // Field from default config.
   public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
 }
